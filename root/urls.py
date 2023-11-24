af34c9d19680eb5c6cb247c0168b4f28d597ee8b
khasanjon <khasanjon.dev@gmail.com>
2023-11-24 10:35:42 +0500

    update some actions and models

--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,25 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from root import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Users API",
+        default_version='v1',
+        contact=openapi.Contact(email="khasanjon.dev@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
     path('admin/', admin.site.urls),
+    path('api/', include('apps.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
