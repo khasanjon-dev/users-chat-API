2f28fc657266adbfde7dcec5ee4dc49a2455961e
khasanjon <khasanjon.dev@gmail.com>
2023-11-24 10:19:01 +0500

    update some actions and models

--- root/settings.py
+++ root/settings.py
@@ -16,12 +16,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'rest_framework',
 
     # my apps
     'users',
