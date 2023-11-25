e2bf70909c5cfa36dfdf46df8d5217030c8e580d
khasanjon <khasanjon.dev@gmail.com>
2023-11-26 00:12:50 +0500

    update some files

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, DateTimeField, ImageField, EmailField, BooleanField
+from django.db.models import CharField, DateTimeField, EmailField, BooleanField, ImageField
 
+from root.settings import DEFAULT_USER_IMAGE
 from shared.django.upload_images import upload_image
 
 
@@ -8,13 +9,12 @@ class User(AbstractUser):
     last_name = CharField(max_length=150, null=True, blank=True)
     bio = CharField(max_length=250, null=True, blank=True)
     email = EmailField(unique=True)
+    image = ImageField(upload_to=upload_image, default=DEFAULT_USER_IMAGE)
     # bool
     is_active = BooleanField(default=False)
     # date
     updated_at = DateTimeField(auto_now=True, null=True)
     date_joined = DateTimeField(auto_now_add=True)
-    # file
-    image = ImageField(upload_to=upload_image)
 
     def __str__(self):
         return self.get_full_name()
