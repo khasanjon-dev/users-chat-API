5431ef0708a54b667c240b490af22ebacfe8c437
khasanjon <khasanjon.dev@gmail.com>
2023-11-26 00:12:49 +0500

    update some files

--- root/urls.py
+++ root/urls.py
@@ -1,10 +1,9 @@
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-
-from root import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,5 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.urls'))
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
