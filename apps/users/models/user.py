bf40497376ee320c0e00a072f559050a36203b5c
khasanjon <khasanjon.dev@gmail.com>
2023-11-25 15:23:17 +0500

    add some actions and update some files

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, DateTimeField, ImageField, EmailField
+from django.db.models import CharField, DateTimeField, ImageField, EmailField, BooleanField
 
 from users.utils.upload_images import upload_image
 
@@ -8,6 +8,8 @@ class User(AbstractUser):
     last_name = CharField(max_length=150, null=True, blank=True)
     bio = CharField(max_length=250, null=True, blank=True)
     email = EmailField(unique=True)
+    # bool
+    is_active = BooleanField(default=False)
     # date
     updated_at = DateTimeField(auto_now=True, null=True)
     date_joined = DateTimeField(auto_now_add=True)
