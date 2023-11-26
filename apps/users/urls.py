07c8f243f7ee43b09b6b3dead1364d1285acc115
khasanjon <khasanjon.dev@gmail.com>
2023-11-26 12:07:56 +0500

    ready update user API

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from users.views import UserViewSet
+from users.views import UserViewSet, CustomTokenRefreshView, CustomTokenObtainPairView
 
 router = DefaultRouter()
 router.register('', UserViewSet, 'user')
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('refresh/', CustomTokenRefreshView.as_view(), name='refresh'),
     path('', include(router.urls))
 ]
