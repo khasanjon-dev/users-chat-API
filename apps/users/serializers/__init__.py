cd1556ab0814db0ebcfe5b3ba8bf757b72190bb2
khasanjon <khasanjon.dev@gmail.com>
2023-11-26 23:13:46 +0500

    add change password API

--- apps/users/serializers/__init__.py
+++ apps/users/serializers/__init__.py
@@ -1,3 +1,3 @@
 from .activation import SendEmailLinkSerializer
 from .auth import RegisterModelSerializer
-from .user import UserModelSerializer, UpdateModelSerializer
+from .user import UserModelSerializer, UpdateModelSerializer, ChangeUsernameSerializer, ChangePasswordSerializer
