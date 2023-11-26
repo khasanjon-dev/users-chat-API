bb6cb7ef7027a46981eb3413b5a765de3bd5075d
khasanjon <khasanjon.dev@gmail.com>
2023-11-26 12:07:55 +0500

    ready update user API

---
+++ apps/users/views/auth.py
@@ -0,0 +1,25 @@
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework_simplejwt.views import TokenViewBase
+
+
+class CustomTokenObtainPairView(TokenViewBase):
+    """
+    login ya'ni access va refresh token uchun
+
+    ```
+    Takes a set of user credentials and returns an access and refresh JSON web
+    token pair to prove the authentication of those credentials.
+    """
+
+    _serializer_class = api_settings.TOKEN_OBTAIN_SERIALIZER
+
+
+class CustomTokenRefreshView(TokenViewBase):
+    """
+    access token olish from refresh token
+
+    Takes a refresh type JSON web token and returns an access type JSON web
+    token if the refresh token is valid.
+    """
+
+    _serializer_class = api_settings.TOKEN_REFRESH_SERIALIZER
