d2bde0387f3c5db97d1660ddf758ff318a0b2ed0
khasanjon <khasanjon.dev@gmail.com>
2023-11-27 13:32:09 +0500

    update some actions

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from users.views import UserViewSet, CustomTokenRefreshView, CustomTokenObtainPairView
-from users.views.activation import ActivateEmail, SendEmailLink
+from users.views import UserViewSet, CustomTokenRefreshView, CustomTokenObtainPairView, ActivateEmailAPIView, \
+    SendEmailLinkAPIView
 
 router = DefaultRouter()
 router.register('', UserViewSet, 'user')
@@ -10,7 +10,7 @@ router.register('', UserViewSet, 'user')
 urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('refresh/', CustomTokenRefreshView.as_view(), name='refresh'),
-    path('activate/<str:uid>/<str:token>/', ActivateEmail.as_view(), name='activate'),
-    path('activate/send-link/', SendEmailLink.as_view(), name='send_link'),
+    path('activate/<str:uid>/<str:token>/', ActivateEmailAPIView.as_view(), name='activate'),
+    path('activate/send-link/', SendEmailLinkAPIView.as_view(), name='send_link'),
     path('', include(router.urls))
 ]
