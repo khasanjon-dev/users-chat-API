a9d16e8523ab70540bb5b7724ec5b9c087e259fa
khasanjon <khasanjon.dev@gmail.com>
2023-11-28 00:03:00 +0500

    add new password html page and some actions

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from users.views import UserViewSet, CustomTokenRefreshView, CustomTokenObtainPairView, ActivateEmailAPIView, \
-    SendEmailLinkAPIView
+    SendEmailLinkAPIView, ResetPasswordAPIView
 
 router = DefaultRouter()
 router.register('', UserViewSet, 'user')
@@ -10,7 +10,9 @@ router.register('', UserViewSet, 'user')
 urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('refresh/', CustomTokenRefreshView.as_view(), name='refresh'),
+
     path('activate/<str:uid>/<str:token>/', ActivateEmailAPIView.as_view(), name='activate'),
     path('activate/send-link/', SendEmailLinkAPIView.as_view(), name='send_link'),
+    path('reset-password/<str:uid>/<str:token>/', ResetPasswordAPIView.as_view(), name='reset_password'),
     path('', include(router.urls))
 ]
