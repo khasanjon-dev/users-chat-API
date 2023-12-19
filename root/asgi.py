4444c44c4114355bc79e80093730642e78ad2b63
khasanjon <khasanjon.dev@gmail.com>
2023-12-19 18:32:58 +0500

    fix some problem

--- root/asgi.py
+++ root/asgi.py
@@ -3,8 +3,8 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from chats.utils.jwt_auth_middleware_stack import JWTAuthMiddlewareStack
-from chats.ws.routing import websocket_urls
+from apps.chats.utils.jwt_auth_middleware_stack import JWTAuthMiddlewareStack
+from apps.chats.ws.routing import websocket_urls
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
