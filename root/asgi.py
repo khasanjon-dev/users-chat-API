825a21bc25115a936868242b1d66e7863b1c9e67
khasanjon <khasanjon.dev@gmail.com>
2023-12-19 19:11:33 +0500

    fix some problem

--- root/asgi.py
+++ root/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 django_asgi_application = get_asgi_application()
 
